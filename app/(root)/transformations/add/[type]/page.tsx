0fc58392afeac17fa3b00c745fcf0b193fd133ed
Aditya <adityak979@gmail.com>
2024-07-03 01:14:32 +0530

    Form Edit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,41 @@
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({
+  params:{ type }
+}:SearchParamProps) => {
+
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if(!userId) redirect('/sign-in');
+
+  const user = await getUserById(userId)
+
   return (
-    <div>
-      <h1>AddTransformationTypePage</h1>
-    </div>
+    <>
+      <Header
+      title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+    
+    <section className='mt-10 ml-5 mr-5'>
+    <TransformationForm 
+         action="Add"
+          userId={userId}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={100} 
+    />
+    </section>
+
+    </>
+
+    
   )
 }
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
+
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
-import qs from "qs"
+import qs from "qs";
 import { twMerge } from "tailwind-merge";
 
 import { aspectRatioOptions } from "@/constants";
@@ -12,19 +13,19 @@ export function cn(...inputs: ClassValue[]) {
 
 // ERROR HANDLER
 export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
-    console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
-  } else if (typeof error === "string") {
-    // This is a string error message
-    console.error(error);
-    throw new Error(`Error: ${error}`);
-  } else {
-    // This is an unknown type of error
-    console.error(error);
-    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
-  }
+  // if (error instanceof Error) {
+  //   // This is a native JavaScript error (e.g., TypeError, RangeError)
+  //   console.error(error.message);
+  //   throw new Error(`Error: ${error.message}`);
+  // } else if (typeof error === "string") {
+  //   // This is a string error message
+  //   console.error(error);
+  //   throw new Error(`Error: ${error}`);
+  // } else {
+  //   // This is an unknown type of error
+  //   console.error(error);
+  //   throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+  // }
 };
 
 // PLACEHOLDER LOADER - while image is transforming
